0a23e457389598bcd5cb819e99307d90df24e9dc
rabeeh <rabeehpuzhakkalakath8547@gmail.com>
2025-05-19 19:51:48 +0530

    folder structer fixed

---
+++ backend/auth_app/mongo.py
@@ -0,0 +1,12 @@
+
+
+
+from pymongo import MongoClient
+import hashlib
+
+client = MongoClient("mongodb://localhost:27017/")
+db = client['authdb']
+users = db['users']
+
+def hash_password(password):
+    return hashlib.sha256(password.encode('utf-8')).hexdigest()
